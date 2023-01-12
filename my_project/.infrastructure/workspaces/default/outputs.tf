17bedd8c3e72a87ca9c173ae43690bc1c8b0dfbe
Dan Schultzer <1254724+danschultzer@users.noreply.github.com>
2023-01-11 19:52:36 -0800

    Make Github Actions trigger deployment

--- my_project/.infrastructure/workspaces/default/outputs.tf
+++ my_project/.infrastructure/workspaces/default/outputs.tf
@@ -12,3 +12,13 @@ output "github_actions-AWS_BUILD_ROLE" {
   value       = aws_iam_role.github_actions_ecr.arn
   description = "The ARN of the role that can be assumed by GHA to push images to ECR - set this as AWS_BUILD_ROLE in the GHA variables"
 }
+
+output "github_actions-AWS_DEPLOY_ROLE" {
+  value       = aws_iam_role.github_actions_ecs.arn
+  description = "The ARN of the role that can be assumed by github actions to deploy to ECS - set this as AWS_DEPLOY_ROLE in the GHA variables"
+}
+
+output "github_actions-AWS_SERVICE_NAME" {
+  value       = local.name
+  description = "The name for all AWS services - set this as AWS_SERVICE_NAME in the GHA variables"
+}
